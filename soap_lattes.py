a1cfd85952df04731b7885b125637e7ab2906f3a
meirelescg <geu_costa@outlook.com>
2024-02-09 20:07:00 -0300

    Soap Lattes adm + ufsb

---
+++ soap_lattes.py
@@ -0,0 +1,131 @@
+from zeep import Client
+from datetime import datetime
+import xml.etree.ElementTree as ET
+import os
+import zipfile
+import pandas as pd
+
+import os
+import logging
+from datetime import datetime
+
+import project
+from Dao import sgbdSQL
+
+client = Client("http://servicosweb.cnpq.br/srvcurriculo/WSCurriculo?wsdl")
+# client.transport.session.proxies = {'http': # Proxy da UNEB ,
+#                                    'https':  #Proxy da UNEB}
+
+
+def get_DataAtualização(id: str) -> datetime:
+    # Retorna a data de atualização do CV
+
+    resultado = client.service.getDataAtualizacaoCV(id)
+
+    if resultado != None:
+        return datetime.strptime(resultado, "%d/%m/%Y %H:%M:%S")
+
+
+def last_update(xml_filename):
+    tree = ET.parse(dir + "atual/" + xml_filename)
+    root = tree.getroot()
+    lista = [
+        i
+        for i in root.items()
+        if i[0] == "DATA-ATUALIZACAO" or i[0] == "HORA-ATUALIZACAO"
+    ]
+    return datetime.strptime(lista[0][1] + lista[1][1], "%d%m%Y%H%M%S")
+
+
+def getIdentificadorCNPQ(nome, data):
+    # Retorna o identificador do CNPQ
+    resultado = client.service.getIdentificadorCNPq(
+        nomeCompleto=nome, dataNascimento=data, cpf=""
+    )
+    if resultado != None:
+        return resultado
+
+
+def salvarCV(id, dir):
+    data = get_DataAtualização(id)
+    print(data)
+    try:
+        if data <= last_update(id + ".xml"):
+            # print('Currículo já está atualizado')
+            logger.debug("Currículo já está atualizado id:" + str(id))
+            return
+    except:
+        # print('Currículo não  atualizado id:'+id)
+        logger.debug("Currículo não  atualizado id:" + str(id))
+
+    print(id)
+    try:
+        resultado = client.service.getCurriculoCompactado(id)
+        arquivo = open(dir + "/zip/" + id + ".zip", "wb")
+        arquivo.write(resultado)
+        arquivo.close()
+
+        with zipfile.ZipFile(dir + "/zip/" + id + ".zip", "r") as zip_ref:
+            zip_ref.extractall(dir)
+            zip_ref.extractall(dir + "/atual")
+            if os.path.exists(id + ".zip"):
+                os.remove(id + ".zip")
+    except:
+        #    print('----------Err:Currículo não  existe')
+        logger.error("----------Err:Currículo não  existe")
+
+
+if __name__ == "__main__":
+    Log_Format = "%(levelname)s %(asctime)s - %(message)s"
+
+    logging.basicConfig(
+        filename="logfile_SOAP_Lattes.log",
+        filemode="w",
+        format=Log_Format,
+        level=logging.DEBUG,
+    )
+
+    logger = logging.getLogger()
+
+    # Testing our Logger
+
+    logger.debug("Inicio")
+
+    dir = "/home/eduardomfjorge/hop/config/projects/Jade-Extrator-Hop/metadata/dataset/xml/curriculos/"
+
+    for f in os.listdir(dir):
+        try:
+            os.remove(os.path.join(dir, f))
+        except:
+            logger.error("Dir")
+
+    logger.debug("Arquivos XML removidos")
+
+    df = pd.read_excel(r"files/pesquisadoresCimatec_v1.xlsx")
+    # df = pd.read_excel(r'files/PesquisadoresProfnit.xlsx')
+    print(df)
+    LATTES_ID = 0
+
+    x = 0
+    for i, infos in df.iterrows():
+
+        print("teste x " + str(infos[LATTES_ID]))
+        print(len(str(infos[LATTES_ID])))
+        if len(str(infos[LATTES_ID])) == 14:
+            lattes_id = "00" + str(infos[LATTES_ID])
+        elif len(str(infos[LATTES_ID])) == 15:
+            lattes_id = "0" + str(infos[LATTES_ID])
+        else:
+            lattes_id = str(infos[LATTES_ID])
+
+        salvarCV(
+            lattes_id,
+            "/home/eduardomfjorge/hop/config/projects/Jade-Extrator-Hop/metadata/dataset/xml/curriculos",
+        )
+        x = x + 1
+    print("Fim " + str(x))
+    logger.debug("Fim Total:" + str(x))
+
+    print("------------Registro:" + str(x))
+    print("------------Fim " + str(x))
+    # salvarCV('5674134492786099','/home/eduardomfjorge/teste/curriculos')
---
+++ soap_lattes_adm.py
@@ -0,0 +1,128 @@
+from zeep import Client
+from datetime import datetime
+import xml.etree.ElementTree as ET
+import os
+import zipfile
+import pandas as pd
+
+import os
+import sys
+import logging
+from datetime import datetime
+import Dao.sgbdSQL as db
+import project as project
+
+
+def get_data_att(id: str) -> datetime:
+    resultado = client.service.getDataAtualizacaoCV(id)
+    if resultado != None:
+        return datetime.strptime(resultado, "%d/%m/%Y %H:%M:%S")
+
+
+def last_update(xml_filename):
+    tree = ET.parse(f"{dir}/atual/{xml_filename}")
+    root = tree.getroot()
+    lista = [
+        i
+        for i in root.items()
+        if i[0] == "DATA-ATUALIZACAO" or i[0] == "HORA-ATUALIZACAO"
+    ]
+    return datetime.strptime(lista[0][1] + lista[1][1], "%d%m%Y%H%M%S")
+
+
+def get_id_cnpq(name: str = str(), date: str = str(), CPF: str = str()):
+    resultado = client.service.getIdentificadorCNPq(
+        nomeCompleto=name, dataNascimento=date, cpf=CPF
+    )
+    if resultado != None:
+        return resultado
+
+
+def salvarCV(id, dir):
+    try:
+        data = get_data_att(id)
+        if data <= last_update(f"{id}.xml"):
+            print("Currículo já está atualizado id:" + str(id))
+            logger.debug("Currículo já está atualizado id:" + str(id))
+            return
+    except:
+        print("\nCurrículo não  atualizado id:" + str(id))
+        logger.debug("\nCurrículo não  atualizado id:" + str(id))
+
+    try:
+        resultado = client.service.getCurriculoCompactado(id)
+        arquivo = open(f"{dir}/zip/{id}.zip", "wb")
+        arquivo.write(resultado)
+        arquivo.close()
+
+        with zipfile.ZipFile(f"{dir}/zip/{id}.zip", "r") as zip_ref:
+            zip_ref.extractall(dir)
+            zip_ref.extractall(f"{dir}/atual")
+            if os.path.exists(f"{id}.zip"):
+                os.remove(f"{id}.zip")
+    except:
+        logger.error("\nErro: Currículo não  existe")
+
+
+def get_researcher_adm_simcc():
+    project.project_env = "8"
+
+    script_sql = """
+        SELECT 
+            name,
+            lattes_id
+        FROM
+            researcher;
+        """
+    registry = db.consultar_db(script_sql)
+
+    df = pd.DataFrame(registry, columns=["name", "lattes_id"])
+
+    return df
+
+if __name__ == "__main__":
+
+    client = Client("http://servicosweb.cnpq.br/srvcurriculo/WSCurriculo?wsdl")
+
+    Log_Format = "%(levelname)s %(asctime)s - %(message)s"
+    logging.basicConfig(
+        filename="logfile_soap_lattes_adm.log",
+        filemode="w",
+        format=Log_Format,
+        level=logging.DEBUG,
+    )
+    logger = logging.getLogger()
+    logger.debug("Inicio")
+
+    dir = "/home/ejorge/hop/config/projects/Jade-Extrator-Hop/metadata/dataset/xml"
+
+    for files in os.listdir(dir):
+        try:
+            os.remove(os.path.join(dir, files))
+        except:
+            logger.error("Erro 003: Directory")
+    logger.debug("Arquivos XML removidos")
+
+    quant_curriculos = 0
+
+    df = get_researcher_adm_simcc()
+
+    for Index, Data in df.iterrows():
+
+        print(f"Curriculo número: {quant_curriculos}\nID do pesquisador: {Data["lattes_id"]}")
+        
+        lattes_id = str(Data["lattes_id"])
+        
+        if len(str(Data["lattes_id"])) == 14:
+            lattes_id = "00" + str(Data["lattes_id"])
+        elif len(str(Data["lattes_id"])) == 15:
+            lattes_id = "0" + str(Data["lattes_id"])
+
+        salvarCV(
+            lattes_id,
+            dir,
+        )
+        quant_curriculos += 1
+
+    logger.debug(f"FIM: {str(quant_curriculos)}")
+    print(f"FIM, Quantidade de curriculos processados: {str(quant_curriculos)}")
