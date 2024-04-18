5f8f860d9979901fe9139d67451a118b3c5e2e1a
meirelescg <geu_costa@outlook.com>
2024-04-18 15:55:06 -0300

    openalex(3)

--- mining.py
+++ mining.py
@@ -16,9 +16,8 @@ doi = sgbdSQL.consultar_db(
 )
 downloaded_productions = os.listdir("Files/json_doi/")
 
-print(doi, downloaded_productions)
 for Data in doi:
-    if f"{Data[1]}.json" in downloaded_productions:
+    if f"{Data[1]}.json" not in downloaded_productions:
         response = requests.get(f"{url}{Data[0]}")
         if response.status_code == 200:
             with open(f"Files/json_doi/{Data[1]}.json", "w") as arquivo:
