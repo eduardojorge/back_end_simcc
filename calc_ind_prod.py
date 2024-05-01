906b1dff35fcc5ec9aa1c9bb951f318b1deacb2b
meirelescg <geu_costa@outlook.com>
2024-05-01 00:25:57 -0300

    IndProd(1)

--- calc_ind_prod.py
+++ calc_ind_prod.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from numpy import NaN
 
 import project as project
 from Dao import sgbdSQL
@@ -16,21 +17,29 @@ weights = {
     "B4": 0.125,
     "BOOK": 1,
     "BOOK_CHAPTER": 0.25,
+    "PATENT": 1,
+    "SOFTWARE": 0.25,
 }
+
+year = list(range(2008, 2025))
+
 
 script_sql = """
     SELECT 
         id, 
         name 
     FROM 
-        researcher 
+        researcher
+    LIMIT 2
     """
 
 registry = sgbdSQL.consultar_db(script_sql)
 
 df_researchers = pd.DataFrame(registry, columns=["id", "name"])
 
-for Index, Data in df_researchers.iterrows():
+
+def article_prod(Data):
+
     script_sql = f"""
         SELECT
             year,
@@ -54,27 +63,31 @@ for Index, Data in df_researchers.iterrows():
     df_ind_prod_base_article = pd.DataFrame(
         registry, columns=["year", "qualis", "count_article"]
     )
-
     df_ind_prod_base_article["ind_prod_article"] = (
         df_ind_prod_base_article["qualis"].map(weights)
         * df_ind_prod_base_article["count_article"]
     )
-    df_ind_prod_base_article = df_ind_prod_base_article.groupby("year")[
+
+    df_ind_prod_base_article = df_ind_prod_base_article.groupby("year", as_index=False)[
         "ind_prod_article"
     ].sum()
+    df_ind_prod_base_article["year"] = df_ind_prod_base_article["year"].astype(int)
+    return df_ind_prod_base_article
 
+
+def book_prod(Data):
     script_sql = f"""
-        SELECT 
+        SELECT
             year,
             COUNT(*) AS count_book
-        FROM 
+        FROM
             public.bibliographic_production bp
         WHERE
-            researcher_id = '{Data['id']}' 
+            researcher_id = '{Data['id']}'
             AND type = 'BOOK'
-        GROUP BY 
+        GROUP BY
             year
-        ORDER BY 
+        ORDER BY
             year;
         """
 
@@ -86,19 +99,24 @@ for Index, Data in df_researchers.iterrows():
         df_ind_prod_base_book["count_book"] * weights["BOOK"]
     )
     df_ind_prod_base_book = df_ind_prod_base_book.drop("count_book", axis=1)
+    df_ind_prod_base_book["year"] = df_ind_prod_base_book["year"].astype(int)
+    return df_ind_prod_base_book
+
+
+def book_chapter_prod(Data):
 
     script_sql = f"""
-        SELECT 
+        SELECT
             year,
             COUNT(*) AS count_book_chapter
-        FROM 
+        FROM
             public.bibliographic_production bp
         WHERE
-            researcher_id = '{Data['id']}' 
+            researcher_id = '{Data['id']}'
             AND type = 'BOOK_CHAPTER'
-        GROUP BY 
+        GROUP BY
             year
-        ORDER BY 
+        ORDER BY
             year;
         """
 
@@ -114,16 +132,108 @@ for Index, Data in df_researchers.iterrows():
     df_ind_prod_base_book_chapter = df_ind_prod_base_book_chapter.drop(
         "count_book_chapter", axis=1
     )
+    df_ind_prod_base_book_chapter["year"] = df_ind_prod_base_book_chapter[
+        "year"
+    ].astype(int)
+    return df_ind_prod_base_book_chapter
 
-    df_ind_prod = df_ind_prod_base_book.merge(
-        df_ind_prod_base_book_chapter, on="year", how="outer"
-    ).merge(pd.DataFrame(df_ind_prod_base_article), on="year", how="outer")
 
-    for Index, Data_Prod in df_ind_prod.fillna(0).iterrows():
-        script_sql = f"""
-        INSERT INTO public.ind_prod(
-            researcher_id, year, ind_prod_article, ind_prod_book, ind_prod_book_chapter)
-        VALUES ('{Data['id']}', {Data_Prod['year']}, {Data_Prod['ind_prod_article']}, {Data_Prod['ind_prod_book']}, {Data_Prod['ind_prod_book_chapter']});
+def patent_prod(Data):
+    script_sql = f"""
+        SELECT
+            development_year,
+            COUNT(*) as count_patent
+        FROM 
+            patent
+        WHERE
+            researcher_id = '{Data['id']}'
+        GROUP BY 
+            researcher_id, development_year
+        """
+    registry = sgbdSQL.consultar_db(script_sql)
+
+    df_ind_prod_base_patent = pd.DataFrame(registry, columns=["year", "count_patent"])
+
+    df_ind_prod_base_patent["count_patent"] = (
+        df_ind_prod_base_patent["count_patent"] * weights["PATENT"]
+    )
+    df_ind_prod_base_patent["year"] = df_ind_prod_base_patent["year"].astype(int)
+    return df_ind_prod_base_patent
+
+
+def software_prod(Data):
+    script_sql = f"""
+        SELECT
+            year,
+            COUNT(*) as count_software
+        FROM 
+            public.software
+        WHERE
+            researcher_id = '{Data['id']}'
+        GROUP BY
+            researcher_id, year
         """
 
-        sgbdSQL.execScricpt_db(script_sql)
+    registry = sgbdSQL.consultar_db(script_sql)
+
+    df_ind_prod_base_software = pd.DataFrame(
+        registry, columns=["year", "count_software"]
+    )
+
+    df_ind_prod_base_software["count_software"] = (
+        df_ind_prod_base_software["count_software"] * weights["SOFTWARE"]
+    )
+    df_ind_prod_base_software["year"] = df_ind_prod_base_software["year"].astype(int)
+    return df_ind_prod_base_software
+
+
+if __name__ == "__main__":
+
+    for Index, Data in df_researchers.iterrows():
+
+        data_frame = pd.DataFrame(year, columns=["year"])
+
+        df = article_prod(Data=Data)
+
+        if not df.empty:
+            data_frame = pd.merge(
+                data_frame, article_prod(Data=Data), on="year", how="left"
+            )
+        else:
+            data_frame["ind_prod_article"] = NaN
+
+        df = book_prod(Data=Data)
+
+        if not df.empty:
+            data_frame = pd.merge(data_frame, df, on="year", how="left")
+        else:
+            data_frame["ind_prod_book"] = NaN
+
+        df = book_chapter_prod(Data=Data)
+
+        if not df.empty:
+            data_frame = pd.merge(data_frame, df, on="year", how="left")
+        else:
+            data_frame["ind_prod_book_chapter"] = NaN
+
+        df = patent_prod(Data=Data)
+
+        if not df.empty:
+            data_frame = pd.merge(data_frame, df, on="year", how="left")
+        else:
+            data_frame["ind_prod_patent"] = NaN
+
+        df = software_prod(Data=Data)
+
+        if not df.empty:
+            data_frame = pd.merge(data_frame, df, on="year", how="left")
+        else:
+            data_frame["ind_prod_software"] = NaN
+
+        for Intern_Index, Intern_Data in data_frame.fillna(0).iterrows():
+            script_sql = f"""
+            INSERT INTO public.ind_prod(
+            researcher_id, year, ind_prod_article, ind_prod_book, ind_prod_book_chapter, ind_prod_patent, ind_prod_software)
+            VALUES ('{Data['id']}', {Intern_Data['year']}, {Intern_Data['ind_prod_article']}, {Intern_Data['ind_prod_book']}, {Intern_Data['ind_prod_book_chapter']}, {Intern_Data['ind_prod_patent']}, {Intern_Data['ind_prod_software']});
+            """
+            sgbdSQL.execScript_db(script_sql)
