7c471ed6907bdca10a58abc717e87cc5ae57ff0e
meirelescg <geu_costa@outlook.com>
2024-05-20 10:20:33 -0300

    Testes de troca de ambiente

--- project.py
+++ project.py
@@ -1,8 +1,8 @@
-project_env = "1"
+project_env = "4"
 
 
 def getProject():
     global project_env
     if project_env == "":
-        project_env = "1"
+        project_env = "4"
     return project_env
