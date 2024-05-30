7d14e902b7f304267ac07f35a09e97f471fa8364
meirelescg <geu_costa@outlook.com>
2024-05-29 22:57:45 -0300

    scrapping openAlex(0)

--- bd_openAlex.py
+++ bd_openAlex.py
@@ -1,26 +1,24 @@
 import json
 import os
 from pprint import pprint
-
 import pandas as pd
 
 import project
 from Dao import sgbdSQL
 
-if __name__ == "__main__":
 
+def extract_institutions():
     project.project_env = "4"
-    i = 0
     for json_path in os.listdir("Files/json_doi"):
         if json_path.endswith(".json"):
             with open(f"Files/json_doi/{json_path}", "r") as f:
-                try:
-                    data = json.load(f)
-                    for item in data["authorships"]:
-                        pprint(item)
-                    # for item in data["authorships"].items():
-                    #     print(item)
+                data = json.load(f)
+                for author in data["authorships"]:
+                    for institution in author["institutions"]:
+                        pprint(institution)
+                        print("\n")
+            break
 
-                    break
-                except:
-                    print(json_path)
+
+if __name__ == "__main__":
+    extract_institutions()
---
+++ get_openAlex.py
@@ -0,0 +1,77 @@
+import os
+import time
+import json
+import requests
+import project
+from Dao import sgbdSQL as db
+
+
+def scrapping_article_data():
+
+    openAlex_url = "https://api.openalex.org/works/https://doi.org/"
+
+    script_sql = """
+        SELECT 
+            doi, 
+            id 
+        FROM 
+            public.bibliographic_production 
+        WHERE 
+            doi IS NOT NULL;
+        """
+
+    doi_list = db.consultar_db(script_sql)
+    json_articles = os.listdir("Files/openAlex_article/")
+
+    for data in doi_list:
+        if f"{data[1]}.json" not in json_articles:
+            response = requests.get(f"{openAlex_url}{data[0]}")
+            if response.status_code == 200:
+                with open(f"Files/openAlex_article/{data[1]}.json", "w") as arquivo:
+                    json.dump(response.json(), arquivo)
+                print("[201] - CREATED")
+            else:
+                with open(f"Files/json_doi/{data[1]}.json", "w") as arquivo:
+                    arquivo.write(str(response.status_code))
+                print("[404] - NOT FOUND")
+            time.sleep(15)
+        else:
+            print(f"[200] - OK")
+
+
+def scrapping_researcher_data():
+    openAlex_url = "https://api.openalex.org/authors/orcid:"
+
+    script_sql = """
+        SELECT 
+            orcid,
+            id 
+        FROM 
+            researcher 
+        WHERE 
+            orcid IS NOT NULL;
+        """
+
+    doi_list = db.consultar_db(script_sql)
+    json_articles = os.listdir("Files/openAlex_researcher/")
+
+    for data in doi_list:
+        if f"{data[1]}.json" not in json_articles:
+            response = requests.get(f"{openAlex_url}{data[0]}")
+            if response.status_code == 200:
+                with open(f"Files/openAlex_researcher/{data[1]}.json", "w") as arquivo:
+                    json.dump(response.json(), arquivo)
+                print("[201] - CREATED")
+            else:
+                with open(f"Files/json_doi/{data[1]}.json", "w") as arquivo:
+                    arquivo.write(str(response.status_code))
+                print("[404] - NOT FOUND")
+            time.sleep(15)
+        else:
+            print(f"[200] - OK")
+
+
+if __name__ == "__main__":
+    project.project_env = "4"
+    scrapping_researcher_data()
+    scrapping_article_data()
