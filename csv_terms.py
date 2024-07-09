1c5f428a7cd6b9808394305888d6c421dd4f2c57
meirelescg <geu_costa@outlook.com>
2024-07-09 15:23:34 -0300

    inserção de dados no firebase

---
+++ csv_terms.py
@@ -0,0 +1,69 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+from Dao import sgbdSQL
+import pandas as pd
+
+# Caminho para o arquivo JSON com as credenciais
+cred = credentials.Certificate("path/to/your/credentials.json")
+firebase_admin.initialize_app(cred)
+
+# Conectando ao Firestore
+db = firestore.client()
+script_sql = """
+    SELECT term, frequency, type_, '0' as great_area, unaccent(LOWER(term)) AS term_normalize
+    FROM public.research_dictionary d
+    WHERE term ~ '^[^0-9]+$'
+        AND CHAR_LENGTH(d.term) >= 4
+        AND frequency >= 8
+        AND type_ NOT IN ('BOOK', 'PATENT')
+
+    UNION
+
+    SELECT term, frequency, type_, '0', unaccent(LOWER(term)) AS term_normalize
+    FROM public.research_dictionary d
+    WHERE term ~ '^[^0-9]+$'
+        AND CHAR_LENGTH(d.term) >= 4
+        AND type_ IN ('BOOK', 'PATENT')
+
+    UNION
+
+    SELECT LOWER(NAME), 1, 'NAME', '0', '0'
+    FROM researcher
+
+    UNION
+
+
+    SELECT 
+        area,
+        1,
+        'AREA',
+        great_area,
+        unaccent(LOWER(great_area)) AS term_normalize
+    FROM 
+        (SELECT
+            LOWER(TRIM(STRING_TO_TABLE(SPLIT_PART(area_specialty, '|', 1), ';'))) as area,
+            LOWER(TRIM(SPLIT_PART(area_specialty, '|', 2), ';')) as great_area
+        FROM public.researcher_production
+        ORDER BY area)
+    """
+registry = sgbdSQL.consultar_db(script_sql)
+
+termos_busca = pd.DataFrame(
+    registry,
+    columns=[
+        "frequency",
+        "great_area",
+        "term",
+        "coletivos",
+        "type_",
+    ],
+)
+# Lista de documentos a serem adicionados
+
+# Referência à coleção
+termos_busca_ref = db.collection("termos_busca")
+
+
+termos_busca_ref.add(termos_busca.to_dict(orient="records"))
+
+print("Documentos adicionados com sucesso.")
