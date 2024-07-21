b90e6b1ade02a720b3ea4eb95464946d10f65f77
meirelesgc <geu_costa@outlook.com>
2024-07-20 21:54:01 -0300

    ...

--- Dao/ufmgSQL.py
+++ Dao/ufmgSQL.py
@@ -7,9 +7,18 @@ def get_rt_list():
     script_sql = """
     SELECT matric, rt FROM ufmg_teacher
     """
-
     registry = sgbdSQL.consultar_db(script_sql)
 
-    data_frame = pd.DataFrame(registry, columns=["matric", "rt"])
+    data_frame_teacher = pd.DataFrame(registry, columns=["matric", "rt"])
 
-    return data_frame.to_dict(orient="records")
+    script_sql = """
+    SELECT matric, rt FROM ufmg_technician
+    """
+    registry = sgbdSQL.consultar_db(script_sql)
+
+    data_frame_technician = pd.DataFrame(registry, columns=["matric", "rt"])
+
+    return {
+        "rt_teacher": data_frame_teacher.to_dict(orient="records"),
+        "rt_technician": data_frame_technician.to_dict(orient="records"),
+    }
