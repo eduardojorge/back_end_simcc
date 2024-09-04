c8f8057caf15b061ee50a9256047b5551d44fcd1
meirelesgc <geu_costa@outlook.com>
2024-09-04 11:47:20 -0300

    correção nos termos

--- csv_terms.py
+++ csv_terms.py
@@ -13,41 +13,75 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 script_sql = """
-    SELECT term, frequency, type_, '0' as great_area, unaccent(LOWER(term)) AS term_normalize
-    FROM public.research_dictionary d
-    WHERE term ~ '^[^0-9]+$'
+    SELECT
+        term,
+        frequency,
+        type_,
+        '0' AS great_area,
+        unaccent(LOWER(term)) AS term_normalize
+    FROM
+        public.research_dictionary d
+    WHERE
+        term ~ '^[^0-9]+$'
         AND CHAR_LENGTH(d.term) >= 4
         AND frequency >= 8
         AND type_ NOT IN ('BOOK', 'PATENT')
 
     UNION
 
-    SELECT term, frequency, type_, '0', unaccent(LOWER(term)) AS term_normalize
-    FROM public.research_dictionary d
-    WHERE term ~ '^[^0-9]+$'
+    SELECT
+        term,
+        frequency,
+        type_,
+        '0',
+        unaccent(LOWER(term)) AS term_normalize
+    FROM
+        public.research_dictionary d
+    WHERE
+        term ~ '^[^0-9]+$'
         AND CHAR_LENGTH(d.term) >= 4
         AND type_ IN ('BOOK', 'PATENT')
 
     UNION
 
-    SELECT LOWER(NAME), 1, 'NAME', '0', '0'
-    FROM researcher
+    SELECT
+        LOWER(name),
+        1,
+        'NAME',
+        '0',
+        '0'
+    FROM
+        researcher
 
     UNION
 
-
-    SELECT 
-        area,
+    SELECT
+        AREA,
         1,
         'AREA',
         great_area,
         unaccent(LOWER(great_area)) AS term_normalize
+    FROM
+        (
+            SELECT
+                LOWER(TRIM(STRING_TO_TABLE(SPLIT_PART(area_specialty, '|', 1), ';'))) AS AREA,
+                LOWER(TRIM(SPLIT_PART(area_specialty, '|', 2), ';')) AS great_area
+            FROM
+                public.researcher_production
+            ORDER BY
+                AREA
+        ) AS subquery
+
+    UNION
+
+    SELECT
+        name,
+        '0',
+        '0',
+        '0',
+        unaccent(LOWER(name))
     FROM 
-        (SELECT
-            LOWER(TRIM(STRING_TO_TABLE(SPLIT_PART(area_specialty, '|', 1), ';'))) as area,
-            LOWER(TRIM(SPLIT_PART(area_specialty, '|', 2), ';')) as great_area
-        FROM public.researcher_production
-        ORDER BY area)
+        researcher
     """
 
 registry = sgbdSQL.consultar_db(script_sql)
@@ -63,7 +97,7 @@ termos_busca = pd.DataFrame(
     ],
 )
 
-termos_busca_ref = db.collection("termos_busca")
+termos_busca_ref = db.collection("termos_busca_cimatec")
 
 for item in termos_busca.to_dict(orient="records"):
     termos_busca_ref.add(item)
