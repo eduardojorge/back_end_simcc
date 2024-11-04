3653df8b85d422d2e4c6d57009231b41894825f0
meirelesgc <geu_costa@outlook.com>
2024-11-04 17:16:38 -0300

    ...

--- csv_powerBI.py
+++ csv_powerBI.py
@@ -570,6 +570,18 @@ def researcher_production_novo_csv_db():
     df_bd.to_csv(csv_dir + "researcher_production_novo_csv_db.csv")
 
 
+def ufmg_tearcher():
+    SCRIPQ_SQL = """ 
+        SELECT researcher_id, matric, inscufmg, nome, genero, situacao, rt, clas, cargo, classe, ref, titulacao, entradanaufmg, progressao, semester
+        FROM public.ufmg_teacher;
+        """
+    registry = sgbdSQL.consultar_db(SCRIPQ_SQL)
+    data_frame = pd.DataFrame(registry, columns=["researcher_id", "matric", "inscufmg", "nome", "genero", "situacao", "rt", "clas", "cargo", "classe", "ref", "titulacao", "entradanaufmg", "progressao", "semester"])
+    data_frame.to_csv(csv_dir + 'ufmg_tearcher.csv', index=False)
+
+
+
+
 def article_distinct_novo_csv_db():
     sql = """
         SELECT 
@@ -1010,4 +1022,5 @@ def csv_powerBI():
 
 
 if __name__ == "__main__":
-    csv_powerBI()
+    # csv_powerBI()
+    dim_qualis_db()
--- server.py
+++ server.py
@@ -52,7 +52,7 @@ from csv_powerBI import (
     dim_graduate_program_student_year_unnest,
     graduate_program_student_researcher_csv_db,
     save_data_to_csv,
-    fat_departament_csv_bd
+    fat_departament_csv_bd,
 )
 
 client = Client("http://servicosweb.cnpq.br/srvcurriculo/WSCurriculo?wsdl")
@@ -580,10 +580,35 @@ def load_save_data_to_csv():
     path = "Files/indicadores_simcc/data.csv"
     return send_file(path, as_attachment=True)
 
+
 @app.route("/dim_departament.csv", methods=["GET"])
 def load_fat_departament_csv_bd():
     fat_departament_csv_bd()
     path = "Files/indicadores_simcc/dim_departament.csv"
+    return send_file(path, as_attachment=True)
+
+
+@app.route("/Qualis.xlsx", methods=["GET"])
+def load_Qualis():
+    path = "Files/indicadores_simcc/Qualis.xlsx"
+    return send_file(path, as_attachment=True)
+
+
+@app.route("/platform_image.xlsx", methods=["GET"])
+def load_platform_image():
+    path = "Files/indicadores_simcc/platform_image.xlsx"
+    return send_file(path, as_attachment=True)
+
+
+@app.route("/DimensaoTipoProducao.xlsx", methods=["GET"])
+def load_DimensaoTipoProducao():
+    path = "Files/indicadores_simcc/DimensaoTipoProducao.xlsx"
+    return send_file(path, as_attachment=True)
+
+
+@app.route("/DimensaoAno.xlsx", methods=["GET"])
+def load_DimensaoAno():
+    path = "Files/indicadores_simcc/DimensaoAno.xlsx"
     return send_file(path, as_attachment=True)
 
 
