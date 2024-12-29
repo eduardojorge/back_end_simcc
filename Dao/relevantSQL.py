e89659a5c31e02988c85c12e48cd26cd0705d01c
meirelesgc <geu_costa@outlook.com>
2024-12-29 17:47:23 -0300

    Alteração no funcionamento das produções relevantes

--- Dao/relevantSQL.py
+++ Dao/relevantSQL.py
@@ -1,39 +1,312 @@
 import pandas as pd
 import Dao.sgbdSQL as db
 
-def add_relevant_production(researcher_id, production_id, type_, has_image):
-    SCRIPT_SQL = """
-        INSERT INTO public.relevant_production (researcher_id, production_id, type, has_image)
-        VALUES (%s, %s, %s, %s)
-        ON CONFLICT (researcher_id, production_id, type)
-        DO UPDATE SET has_image = true;
-        """
-    db.execScript_db(SCRIPT_SQL, [researcher_id, production_id, type_, has_image])
-    
 
-def get_relevant_list(researcher_id, type_):
-    params = []
+def add_image(id, type_):
+    bibliographic_production = [
+        "BOOK",
+        "BOOK_CHAPTER",
+        "ARTICLE",
+        "WORK_IN_EVENT",
+        "TEXT_IN_NEWSPAPER_MAGAZINE",
+    ]
+    if type_ in bibliographic_production:
+        add_bibliographic_production_image(id, type_)
+    if type_ == "SOFTWARE":
+        add_software_image(id)
+    if type_ == "PATENT":
+        add_patent_image(id)
+    if type_ == "BRAND":
+        add_brand_image(id)
+
+
+def add_brand_image(id):
     SCRIPT_SQL = """
-        SELECT production_id, type, has_image, created_at
-        FROM public.relevant_production
-        WHERE 1 = 1
+    UPDATE brand
+    SET has_image = true
+    WHERE id = %(id)s;
+    """
+    db.execScript_db(sql=SCRIPT_SQL, params={"id": id})
+
+
+def add_patent_image(id):
+    SCRIPT_SQL = """
+    UPDATE patent
+    SET has_image = true
+    WHERE id = %(id)s;
+    """
+    db.execScript_db(sql=SCRIPT_SQL, params={"id": id})
+
+
+def add_bibliographic_production_image(id, type_):
+    SCRIPT_SQL = """
+        UPDATE bibliographic_production
+        SET has_image = true
+        WHERE id = %(id)s AND type = %(type_)s;
         """
-    
+    db.execScript_db(sql=SCRIPT_SQL, params={"id": id, "type_": type_})
+
+
+def add_software_image(id):
+    SCRIPT_SQL = """
+    UPDATE software
+    SET has_image = true
+    WHERE id = %(id)s;
+    """
+    db.execScript_db(sql=SCRIPT_SQL, params={"id": id})
+
+
+def delete_image(id, type_):
+    bibliographic_production = [
+        "BOOK",
+        "BOOK_CHAPTER",
+        "ARTICLE",
+        "WORK_IN_EVENT",
+        "TEXT_IN_NEWSPAPER_MAGAZINE",
+    ]
+    if type_ in bibliographic_production:
+        delete_bibliographic_production_image(id, type_)
+    if type_ == "SOFTWARE":
+        delete_software_image(id)
+    if type_ == "PATENT":
+        delete_patent_image(id)
+    if type_ == "BRAND":
+        delete_brand_image(id)
+
+
+def delete_brand_image(id):
+    SCRIPT_SQL = """
+    UPDATE brand
+    SET has_image = false
+    WHERE id = %(id)s;
+    """
+    db.execScript_db(sql=SCRIPT_SQL, params={"id": id})
+
+
+def delete_patent_image(id):
+    SCRIPT_SQL = """
+    UPDATE patent
+    SET has_image = false
+    WHERE id = %(id)s;
+    """
+    db.execScript_db(sql=SCRIPT_SQL, params={"id": id})
+
+
+def delete_bibliographic_production_image(id, type_):
+    SCRIPT_SQL = """
+        UPDATE bibliographic_production
+        SET has_image = false
+        WHERE id = %(id)s AND type = %(type_)s;
+        """
+    db.execScript_db(sql=SCRIPT_SQL, params={"id": id, "type_": type_})
+
+
+def delete_software_image(id):
+    SCRIPT_SQL = """
+    UPDATE software
+    SET has_image = false
+    WHERE id = %(id)s;
+    """
+    db.execScript_db(sql=SCRIPT_SQL, params={"id": id})
+
+
+def add_relevance(id, type_):
+    bibliographic_production = [
+        "BOOK",
+        "BOOK_CHAPTER",
+        "ARTICLE",
+        "WORK_IN_EVENT",
+        "TEXT_IN_NEWSPAPER_MAGAZINE",
+    ]
+    if type_ in bibliographic_production:
+        add_bibliographic_production_relevance(id, type_)
+    if type_ == "SOFTWARE":
+        add_software_relevance(id)
+    if type_ == "PATENT":
+        add_patent_relevance(id)
+    if type_ == "BRAND":
+        add_brand_relevance(id)
+
+
+def add_brand_relevance(id):
+    SCRIPT_SQL = """
+    UPDATE brand
+    SET relevance = true
+    WHERE id = %(id)s;
+    """
+    db.execScript_db(sql=SCRIPT_SQL, params={"id": id})
+
+
+def add_patent_relevance(id):
+    SCRIPT_SQL = """
+    UPDATE patent
+    SET relevance = true
+    WHERE id = %(id)s;
+    """
+    db.execScript_db(sql=SCRIPT_SQL, params={"id": id})
+
+
+def add_bibliographic_production_relevance(id, type_):
+    SCRIPT_SQL = """
+        UPDATE bibliographic_production
+        SET relevance = true
+        WHERE id = %(id)s AND type = %(type_)s;
+    """
+    db.execScript_db(sql=SCRIPT_SQL, params={"id": id, "type_": type_})
+
+
+def add_software_relevance(id):
+    SCRIPT_SQL = """
+    UPDATE software
+    SET relevance = true
+    WHERE id = %(id)s;
+    """
+    db.execScript_db(sql=SCRIPT_SQL, params={"id": id})
+
+
+def get_relevant_bibliographic_production(researcher_id, type_):
+    params = {}
+    SCRIPT_SQL = """
+        SELECT id, type, has_image 
+        FROM bibliographic_production
+        WHERE relevance = true
+        """
+
     if researcher_id:
-        SCRIPT_SQL += "AND researcher_id = %s"
-        params.append(researcher_id)
+        SCRIPT_SQL += "AND researcher_id = %(researcher_id)s"
+        params["researcher_id"] = researcher_id
 
     if type_:
-        SCRIPT_SQL += "AND type = %s"
-        params.append(type_)
+        SCRIPT_SQL += "AND type = %(type_)s"
+        params["type_"] = type_
 
-    result = db.consultar_db(SCRIPT_SQL, params)
-    df = pd.DataFrame(result, columns=["production_id", "type", 'has_image', "created_at"])
-    return df.to_dict(orient='records')
+    result = db.consultar_db(sql=SCRIPT_SQL, params=params)
+    df = pd.DataFrame(result, columns=["production_id", "type_", "has_image"])
+    return df
 
-def delete_relevant_production(production_id, type_):
+
+def get_relevant_patent(researcher_id, type_):
+    params = {}
     SCRIPT_SQL = """
-        DELETE FROM public.relevant_production
-        WHERE production_id = %s AND type = %s;
-        """
-    db.execScript_db(SCRIPT_SQL, [production_id, type_])
+    SELECT id, 'PATENT', has_image 
+    FROM patent
+    WHERE relevance = true
+    """
+    if researcher_id:
+        SCRIPT_SQL += "AND researcher_id = %(researcher_id)s"
+        params["researcher_id"] = researcher_id
+
+    result = db.consultar_db(sql=SCRIPT_SQL, params=params)
+    df = pd.DataFrame(result, columns=["production_id", "type_", "has_image"])
+    return df
+
+
+def get_relevant_software(researcher_id, type_):
+    params = {}
+    SCRIPT_SQL = """
+    SELECT id, 'SOFTWARE', has_image 
+    FROM software
+    WHERE relevance = true
+    """
+    if researcher_id:
+        SCRIPT_SQL += "AND researcher_id = %(researcher_id)s"
+        params["researcher_id"] = researcher_id
+
+    result = db.consultar_db(sql=SCRIPT_SQL, params=params)
+    df = pd.DataFrame(result, columns=["production_id", "type_", "has_image"])
+    return df
+
+
+def get_relevant_brand(researcher_id, type_):
+    params = {}
+    SCRIPT_SQL = """
+    SELECT id, 'BRAND', has_image 
+    FROM brand
+    WHERE relevance = true
+    """
+
+    if researcher_id:
+        SCRIPT_SQL += "AND researcher_id = %(researcher_id)s"
+        params["researcher_id"] = researcher_id
+
+    result = db.consultar_db(sql=SCRIPT_SQL, params=params)
+    df = pd.DataFrame(result, columns=["production_id", "type_", "has_image"])
+    return df
+
+
+def get_relevant_list(researcher_id, type_):
+    relevant_production = []
+    bibliographic_production = [
+        "BOOK",
+        "BOOK_CHAPTER",
+        "ARTICLE",
+        "WORK_IN_EVENT",
+        "TEXT_IN_NEWSPAPER_MAGAZINE",
+    ]
+    if type_ in bibliographic_production:
+        relevant_production.append(
+            get_relevant_bibliographic_production(researcher_id, type_)
+        )
+    if type_ == "SOFTWARE":
+        relevant_production.append(get_relevant_software(researcher_id, type_))
+    if type_ == "PATENT":
+        relevant_production.append(get_relevant_patent(researcher_id, type_))
+    if type_ == "BRAND":
+        relevant_production.append(get_relevant_brand(researcher_id, type_))
+
+    df = pd.concat(relevant_production, ignore_index=True)
+    return df.to_dict(orient="records")
+
+
+def delete_relevance(id, type_):
+    bibliographic_production = [
+        "BOOK",
+        "BOOK_CHAPTER",
+        "ARTICLE",
+        "WORK_IN_EVENT",
+        "TEXT_IN_NEWSPAPER_MAGAZINE",
+    ]
+    if type_ in bibliographic_production:
+        delete_bibliographic_production_relevance(id, type_)
+    if type_ == "SOFTWARE":
+        delete_software_relevance(id)
+    if type_ == "PATENT":
+        delete_patent_relevance(id)
+    if type_ == "BRAND":
+        delete_brand_relevance(id)
+
+
+def delete_brand_relevance(id):
+    SCRIPT_SQL = """
+    UPDATE brand
+    SET relevance = false
+    WHERE id = %(id)s;
+    """
+    db.execScript_db(sql=SCRIPT_SQL, params={"id": id})
+
+
+def delete_patent_relevance(id):
+    SCRIPT_SQL = """
+    UPDATE patent
+    SET relevance = false
+    WHERE id = %(id)s;
+    """
+    db.execScript_db(sql=SCRIPT_SQL, params={"id": id})
+
+
+def delete_bibliographic_production_relevance(id, type_):
+    SCRIPT_SQL = """
+        UPDATE bibliographic_production
+        SET relevance = false
+        WHERE id = %(id)s AND type = %(type_)s;
+    """
+    db.execScript_db(sql=SCRIPT_SQL, params={"id": id, "type_": type_})
+
+
+def delete_software_relevance(id):
+    SCRIPT_SQL = """
+    UPDATE software
+    SET relevance = false
+    WHERE id = %(id)s;
+    """
+    db.execScript_db(sql=SCRIPT_SQL, params={"id": id})
--- Rest/relevantRest.py
+++ Rest/relevantRest.py
@@ -7,26 +7,25 @@ management = Blueprint("managementRest", __name__)
 UPLOAD_FOLDER = os.path.join(os.getcwd(), "Files", "image_productions")
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-@management.route('/image/<id>', methods=["POST"])
-def post_image(id):
-    type_ = request.args.get('type')
 
+@management.route("/image/<id>", methods=["POST"])
+def post_image(id):
+    type_ = request.args.get("type")
     if "file" not in request.files:
         return "No files sent", 400
-
     file = request.files["file"]
     if file.filename is None:
         return "No files selected", 400
-    
     if "." in file.filename:
-        extension = file.filename.rsplit(".", 1)[1]  
+        extension = file.filename.rsplit(".", 1)[1]
     else:
         extension = "jpg"
 
     file_path = os.path.join(UPLOAD_FOLDER, f"{id}.{extension}")
-    relevantSQL.add_relevant_production(id, type_, True)
+    relevantSQL.add_image(id, type_)
     file.save(file_path)
     return "Image saved successfully", 200
+
 
 @management.route("/image/<id>", methods=["GET"])
 def get_image(id):
@@ -34,33 +33,37 @@ def get_image(id):
         file_path = os.path.join(UPLOAD_FOLDER, f"{id}.{extension}")
         if os.path.isfile(file_path):
             return send_file(file_path)
-        
+
+
 @management.route("/image/<id>", methods=["DELETE"])
 def delete_image(id):
-    type_ = request.args.get('type')
+    type_ = request.args.get("type")
     for extension in ["jpg", "jpeg", "png", "gif"]:
         file_path = os.path.join(UPLOAD_FOLDER, f"{id}.{extension}")
         if os.path.isfile(file_path):
             os.remove(file_path)
-            relevantSQL.delete_relevant_production(id, type_)
+            relevantSQL.delete_image(id, type_)
             return "Image deleted successfully", 200
     return "Image not found", 404
 
+
 @management.route("/relevant/<researcher_id>/<production_id>", methods=["POST"])
 def post_relevant_production(researcher_id, production_id):
-    type_ = request.args.get('type')
-    relevantSQL.add_relevant_production(researcher_id, production_id, type_, False)
-    return 'OK', 200
+    type_ = request.args.get("type")
+    relevantSQL.add_relevance(production_id, type_)
+    return "OK", 200
+
 
 @management.route("/relevant", methods=["GET"])
 def get_relevant_production_list():
-    researcher_id = request.args.get('researcher_id')
-    type_ = request.args.get('type')
+    researcher_id = request.args.get("researcher_id")
+    type_ = request.args.get("type")
     relevant_list = relevantSQL.get_relevant_list(researcher_id, type_)
     return relevant_list
 
+
 @management.route("/relevant/<id>", methods=["DELETE"])
 def delete_relevant_production(id):
-    type_ = request.args.get('type')
-    relevantSQL.delete_relevant_production(id, type_)
-    return 'OK', 200
+    type_ = request.args.get("type")
+    relevantSQL.delete_relevance(id, type_)
+    return "OK", 200
