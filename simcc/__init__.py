87d96af105a3facef95ac3fc60de2632ac4cbbf8
meirelesgc <geu_costa@outlook.com>
2024-12-31 15:54:39 -0300

    Configurando o Proxy para Retrocompatibilidade

--- simcc/__init__.py
+++ simcc/__init__.py
@@ -1,8 +1,34 @@
-from fastapi import FastAPI
+from http import HTTPStatus
+
+import httpx
+from fastapi import FastAPI, Request, Response
 
 app = FastAPI()
 
+PROXY_URL = 'http://localhost:8080'
 
-@app.get("/")
+
+@app.get('/')
 def read_root():
-    return {"message": "Olá Mundo!"}
+    return {'message': 'Olá Mundo!'}
+
+
+@app.middleware('http')
+async def reverse_proxy(request: Request, call_next):
+    response = await call_next(request)
+
+    if response.status_code == HTTPStatus.NOT_FOUND:
+        async with httpx.AsyncClient() as client:
+            proxy_response = await client.request(
+                method=request.method,
+                url=f'{PROXY_URL}{request.url.path}',
+                params=request.query_params,
+                headers=dict(request.headers),
+                content=await request.body(),
+            )
+            return Response(
+                content=proxy_response.content,
+                status_code=proxy_response.status_code,
+                headers=dict(proxy_response.headers),
+            )
+    return response
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,40 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
+import httpx
+import respx
 
-from simcc import app
+from simcc import PROXY_URL
 
 
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
-
+def test_root_route(client):
     response = client.get('/')
-
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+@respx.mock
+def test_reverse_proxy(client):
+    URL = PROXY_URL + '/test'
+    target_route = respx.get(URL).mock(
+        return_value=httpx.Response(200, json={'message': 'Proxy working!'})
+    )
+
+    response = client.get('/test')
+
+    assert target_route.called
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Proxy working!'}
+
+
+@respx.mock
+def test_reverse_proxy_not_found(client):
+    URL = PROXY_URL + '/not-found'
+    target_route = respx.get(URL).mock(
+        return_value=httpx.Response(404, json={'detail': 'Not Found'})
+    )
+
+    response = client.get('/not-found')
+
+    assert target_route.called
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'Not Found'}
