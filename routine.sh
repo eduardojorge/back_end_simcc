1ddf9c11f5db6097367d8c9dc93aaf8e2ec0f582
meirelesgc <geu_costa@outlook.com>
2025-01-24 22:36:38 -0300

    Ajustando o .sh das rotinas

--- routine.sh
+++ routine.sh
@@ -3,12 +3,11 @@
 PROJECT_HOME=$(dirname "$(readlink -f "$0")")
 
 cd "$PROJECT_HOME" || exit 1
-source "${PROJECT_HOME}/.env"
-
-rm -rf $JADE_EXTRATOR_FOLTER/audit/*
+source ".env"
 
 poetry run python routines/soap_lattes.py
 
+rm -rf $JADE_EXTRATOR_FOLTER/audit/*
 "${JADE_EXTRATOR_FOLTER}/hop-run.sh" -r local -j Jade-Extrator-Hop -f "${PROJECT_HOME}/metadata/dataset/workflow/Index.hwf"
 
 poetry run python routines/population.py
@@ -19,4 +18,8 @@ poetry run python routines/production.py
 
 poetry run python routines/lattes_bfs.py
 
-poetry run python routines/researcher_class.py
+poetry run python routines/researcher_indprod.py
+
+poetry run python routines/program_indprod.py
+
+poetry run python routines/powerBI.py
