446a884a219cde7c4c9e4762ed6ace0746768515
meirelesgc <geu_costa@outlook.com>
2025-01-25 01:15:08 -0300

    Coreções na rotina

--- routine.sh
+++ routine.sh
@@ -6,21 +6,21 @@ cd "$PROJECT_HOME" || exit 1
 source .venv/bin/activate
 source .env
 
-poetry run python routines/soap_lattes.py
+.venv/bin/python routines/soap_lattes.py
 
 rm -rf $JADE_EXTRATOR_FOLTER/audit/*
 "${JADE_EXTRATOR_FOLTER}/hop-run.sh" -r local -j Jade-Extrator-Hop -f "${PROJECT_HOME}/metadata/dataset/workflow/Index.hwf"
 
-poetry run python routines/population.py
+.venv/bin/python routines/population.py
 
-poetry run python routines/pog.py
+.venv/bin/python routines/pog.py
 
-poetry run python routines/production.py
+.venv/bin/python routines/production.py
 
-poetry run python routines/lattes_bfs.py
+.venv/bin/python routines/lattes_bfs.py
 
-poetry run python routines/researcher_indprod.py
+.venv/bin/python routines/researcher_indprod.py
 
-poetry run python routines/program_indprod.py
+.venv/bin/python routines/program_indprod.py
 
-poetry run python routines/powerBI.py
+.venv/bin/python routines/powerBI.py
