1300e0f5bae32abdbff483490d05d453e963a054
meirelesgc <geu_costa@outlook.com>
2025-01-25 22:06:57 -0300

    Download de curriculos (part 2)

--- simcc/routers/GenericRouter.py
+++ simcc/routers/GenericRouter.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import FileResponse
 from zeep import Client
 
 router = APIRouter()
@@ -8,6 +9,17 @@ router = APIRouter()
 def getCurriculoCompactado(lattes_id: str):
     client = Client('http://servicosweb.cnpq.br/srvcurriculo/WSCurriculo?wsdl')
     result = client.service.getCurriculoCompactado(lattes_id)
-    file = open(f'storage/{lattes_id}.zip', 'wb')
-    file.write(result)
-    file.close()
+
+    if result:
+        file_path = f'storage/{lattes_id}.zip'
+
+        with open(file_path, 'wb') as file:
+            file.write(result)
+
+        return FileResponse(
+            path=file_path,
+            filename=f'{lattes_id}.zip',
+            media_type='application/zip',
+        )
+
+    raise HTTPException(status_code=404, detail='Curriculum not found')
