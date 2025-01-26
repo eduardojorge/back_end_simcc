cd6dc70795747db87db5aef922ba161963547682
meirelesgc <geu_costa@outlook.com>
2025-01-25 22:22:40 -0300

    Download de curriculos (part 3)

--- simcc/routers/GenericRouter.py
+++ simcc/routers/GenericRouter.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from http import HTTPStatus
+
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import FileResponse
 from zeep import Client
@@ -5,21 +8,34 @@ from zeep import Client
 router = APIRouter()
 
 
-@router.get('/getCurriculoCompactado')
-def getCurriculoCompactado(lattes_id: str):
+@router.get(
+    '/getCurriculoCompactado',
+    response_class=FileResponse,
+    status_code=HTTPStatus.OK,
+)
+def lattes_xml(lattes_id: str):
     client = Client('http://servicosweb.cnpq.br/srvcurriculo/WSCurriculo?wsdl')
-    result = client.service.getCurriculoCompactado(lattes_id)
-
-    if result:
+    response = client.service.getCurriculoCompactado(lattes_id)
+    if response:
         file_path = f'storage/{lattes_id}.zip'
-
         with open(file_path, 'wb') as file:
-            file.write(result)
-
+            file.write(response)
         return FileResponse(
             path=file_path,
             filename=f'{lattes_id}.zip',
             media_type='application/zip',
         )
+    raise HTTPException(status_code=404, detail='Curriculum not found')
 
+
+@router.get(
+    '/getDataAtualizacaoCV',
+    response_class=datetime,
+    status_code=HTTPStatus.OK,
+)
+def current_lattes_date(lattes_id: str):
+    client = Client('http://servicosweb.cnpq.br/srvcurriculo/WSCurriculo?wsdl')
+    response = client.service.getDataAtualizacaoCV(lattes_id)
+    if response:
+        return datetime.strptime(response, '%d/%m/%Y %H:%M:%S')
     raise HTTPException(status_code=404, detail='Curriculum not found')
