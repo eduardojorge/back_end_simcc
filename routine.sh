2002db77c5adea049cbfa0685473e9cbd5ae57db
gmcosta <geu_costa@outlook.com>
2025-02-17 13:44:07 -0300

    Mudanças do conectee

--- routine.sh
+++ routine.sh
@@ -13,20 +13,20 @@ cd "$SIMCC_HOME" || exit 1
 source .venv/bin/activate
 source .env
 
-"${SIMCC_HOME}/.venv/bin/python" routines/soap_lattes.py
-check_error "soap_lattes"
+# "${SIMCC_HOME}/.venv/bin/python" routines/soap_lattes.py
+# check_error "soap_lattes"
 
-rm -rf $JADE_EXTRATOR_FOLTER/audit/*
-"${JADE_EXTRATOR_FOLTER}/hop-run.sh" -r local -j Jade-Extrator-Hop -f "${PROJECT_HOME}/metadata/dataset/workflow/Index.hwf"
+# rm -rf $JADE_EXTRATOR_FOLTER/audit/*
+# "${JADE_EXTRATOR_FOLTER}/hop-run.sh" -r local -j Jade-Extrator-Hop -f "${PROJECT_HOME}/metadata/dataset/workflow/Index.hwf"
 
-"${SIMCC_HOME}/.venv/bin/python" routines/population.py
-check_error "population"
+# "${SIMCC_HOME}/.venv/bin/python" routines/population.py
+# check_error "population"
 
-"${SIMCC_HOME}/.venv/bin/python" routines/pog.py
-check_error "pog"
+# "${SIMCC_HOME}/.venv/bin/python" routines/pog.py
+# check_error "pog"
 
-"${SIMCC_HOME}/.venv/bin/python" routines/production.py
-check_error "production"
+# "${SIMCC_HOME}/.venv/bin/python" routines/production.py
+# check_error "production"
 
 "${SIMCC_HOME}/.venv/bin/python" routines/lattes_bfs.py
 check_error "lattes_bfs"
